814ece6de0bfd91edc0ae59e4e8cdd0fb682187e
JorgeEugenio <er.jorgeluis@gmail.com>
2021-03-24 12:30:53 -0500

    2021-24-03_12.30.49

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//const servicioRoute = require('./routes/servicioRoute')
+const principalRoute = require('./routes/principalRoute')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//app.use('/v1',productsRoutes)
+app.use('/v1',principalRoute)
+
+app.use('/public', express.static(`${__dirname}/storage/imgs/`))
+module.exports = app
